5154e493201422f5a68d457fd3aea759b17a3421
CJ Coffey <cj@cjcoffey.com>
2024-12-18 10:31:12 -0500

    build: use npm over pnpm
    astro couldn't build images???

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run dev',
+    command: 'npm run dev',
     url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
   },
