fe6a63e99bd68e01997591375ffd2c0fa7826bcd
CJ Coffey <cj@cjcoffey.com>
2025-08-23 23:45:46 -0400

    chore: formatting

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
 import markdownIt from 'markdown-it';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 const parser = new markdownIt();
 
@@ -12,15 +12,19 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => {
-      const body = typeof post.body === 'string' ? post.body : '';
-      const renderedBody = parser.render(body);
-      const sanitized = sanitizeHtml(renderedBody, { allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']) });
-      return {
-        link: `/posts/${post.id}/`,
-        content: sanitized,
-        ...post.data,
-      };
-    }).sort((a, b) => b.date.getTime() - a.date.getTime()),
+    items: posts
+      .map((post) => {
+        const body = typeof post.body === 'string' ? post.body : '';
+        const renderedBody = parser.render(body);
+        const sanitized = sanitizeHtml(renderedBody, {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        });
+        return {
+          link: `/posts/${post.id}/`,
+          content: sanitized,
+          ...post.data,
+        };
+      })
+      .sort((a, b) => b.date.getTime() - a.date.getTime()),
   });
 }
